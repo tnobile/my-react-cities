d6c98eb693b78dc88851140adee567c8d8ec45d1
tnobile <takao.nosaka@outlook.com>
2021-05-01 13:47:44 +0200

    different way to fetch async/await

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,22 +7,39 @@ const Home = () => {
     const [data, setData] = useState([]);
     const [country, setCountry] = useState("gb")
     useEffect(() => {
+        // 1. with then
         // getData().then(d => {
         //     console.log("got", d);
         //     setData(d);
         // }).catch(e => { console.log("failed in fetch", e.message) });
-        const fetchData = async () => {
-            try {
-                const result = await getData(country);
-                console.log(`got ${result.length}`)
-                setData(result);
-            } catch (e) {
-                setData([]);
-                console.log(`${e.name}:${e.message}`);
-            }
-        }
 
-        fetchData();
+        // 2. with function
+        // const fetchData = async () => {
+        //     try {
+        //         const result = await getData(country);
+        //         console.log(`got ${result.length}`)
+        //         setData(result);
+        //     } catch (e) {
+        //         setData([]);
+        //         console.log(`${e.name}:${e.message}`);
+        //     }
+        // }
+        // fetchData();
+
+        // 3. immediately executed function for async purpose
+        (async () => {
+            //try {
+            const result = await getData(country);
+            console.log(`got ${result.length}  for ${country}`)
+            setData(result);
+            //} catch (e) {
+            //    setData([]);
+            //    console.log(`${e.name}:${e.message}`);
+            //}
+        })().catch(e => {
+            setData([]);
+            console.error(`${e.name}:${e.message}`)
+        });
     }, [country])
     const handleCountryChange = (evt) => {
         setCountry(evt.target.value);
@@ -32,6 +49,8 @@ const Home = () => {
         <>
             <div>
                 <h1>Country/City Data</h1>
+                <a href='https://tnobile.github.io/data-world-cities/'>source</a>
+                {' '}
                 <select name='country' onChange={handleCountryChange}>
                     <option value="jp" name="jp">Japan</option>
                     <option value="gb" name="gb">UK</option>
@@ -43,13 +62,14 @@ const Home = () => {
                     <option value="fr" name="fr">France</option>
                 </select>
             </div>
-            <table>
-                <tbody>
-                    {data && data.map((d, i) => <City key={i} row={i} city={d}></City>)}
-                    {data && data.length === 0 && <h2>No data for {country}</h2>}
-                </tbody>
-            </table>
-        </>
+            {data && data.length > 0 &&
+                <table>
+                    <tbody>
+                        {data.map((d, i) => <City key={i} row={i} city={d}></City>)}
+                    </tbody>
+                </table>
+            }
+            {data && data.length === 0 && <h2>No data for {country}</h2>} </>
     )
 }
 
